528c40b2feb1a3d6afa83bb0ae9afdeac364ca4a
ee7klt <kltah1@gmail.com>
2019-11-05 14:51:18 +0800

    10 class

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,13 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-    Hello React!
-    </div>
-  );
+class RegularClass {}
+class ComponentClass extends Component {}
+const regularClassInstance = new RegularClass();
+const componentClassInstance = new ComponentClass();
+console.log('regularClassInstance',regularClassInstance);
+console.log('componentClassInstance',componentClassInstance);
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello!</h1>
+        <p>My name is Alex</p>
+        <p>I'm a Machine Learning Engineer</p>
+      </div>
+    )
+  }
 }
 
 export default App;
