f083675be541f7bac2d43e58f7a6de33e4df8d66
ee7klt <kltah1@gmail.com>
2019-11-10 00:51:10 +0800

    setState calls render. don't use mutation

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,19 +1,42 @@
 import React, {Component} from 'react';
 
-class RegularClass {}
-class ComponentClass extends Component {}
-const regularClassInstance = new RegularClass();
-const componentClassInstance = new ComponentClass();
-console.log('regularClassInstance',regularClassInstance);
-console.log('componentClassInstance',componentClassInstance);
+
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state={displayBio: false};
+
+  }
+
+
+  // readMore = () => {
+  //   this.setState({displayBio: true})
+  // }
+  //
+  // readLess = () => {
+  //   this.setState({displayBio: false})
+  // }
+
+  toggleDisplayBio = () => {
+    console.log(this.state)
+    this.setState({displayBio: !this.state.displayBio});
+  }
+
   render() {
+
     return (
       <div>
         <h1>Hello!</h1>
         <p>My name is Alex</p>
         <p>I'm a Machine Learning Engineer</p>
+        {this.state.displayBio ? (  <div>
+            <p>I live in Fukuoka, and I code every day</p>
+            <p>My favorite language is Python</p>
+            <p>Besides coding I like learning new languages</p>
+            <div><button onClick={this.toggleDisplayBio}>Show less</button></div>
+          </div>) : (<div><button onClick={this.toggleDisplayBio}>Read more</button></div>)}
+
       </div>
     )
   }
--- portfolio/src/index.js
+++ portfolio/src/index.js
@@ -4,19 +4,3 @@ import App from './App';
 
 
 ReactDOM.render(<App></App>,document.getElementById('root'));
-
-
-class Animal {
-  constructor(name, age) {
-    this.name = name;
-    this.age = age;
-  }
-
-  speak() {
-    console.log(' I am ', this.name, ' and I am ', this.age, ' years old')
-  }
-}
-
-const animal = new Animal('simba',42)
-animal.speak();
-console.log(animal)
