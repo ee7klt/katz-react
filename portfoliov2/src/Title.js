af37d4e33156d8228fde076fff24891d0961e696
ee7klt <kltah1@gmail.com>
2019-12-04 00:45:54 +0800

    componentWillUnmount clearInverval

---
+++ portfoliov2/src/Title.js
@@ -0,0 +1,44 @@
+import React, {Component} from 'react';
+
+const TITLES = [
+  'a software engineer',
+  'a polyglot',
+  'a traveller'
+];
+
+
+
+
+class Title extends Component {
+  state = {titleIndex: 0};
+
+  componentDidMount() {
+    console.log('Title component has mounted');
+    this.animateTitles();
+  }
+
+  componentWillUnmount() {
+    console.log('Title component will unmount!');
+    clearInterval(this.titleInterval);
+  }
+
+  animateTitles = () => {
+    this.titleInterval = setInterval(()=>{
+      const titleIndex = (this.state.titleIndex +1)%TITLES.length;
+      this.setState({titleIndex})
+    }, 4000);
+    console.log(this.titleInterval);
+  }
+
+
+  render() {
+    const title = TITLES[this.state.titleIndex];
+    return (
+      <p>I am {title}</p>
+    )
+  }
+}
+
+
+
+export default Title;
