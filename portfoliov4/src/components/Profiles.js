979d8099a5c61ee76b1523043e7d0e76cf72f96a
ee7klt <kltah1@gmail.com>
2019-12-23 23:32:10 +0800

    34 fetch and a jokes component

---
+++ portfoliov4/src/components/Profiles.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import SOCIALPROFILES from '../data/socialProfiles';
+
+
+const Profile = props =>  {
+    const {title, description, image, link} = props.profile;
+    return (
+      <div style={{margin:10}} >
+      <a href={link} >
+      <img src= {image} style = {{width: 40}} />
+      </a>
+      </div>
+    )
+}
+
+
+const Profiles = ()=>  (
+      <div>
+        <h2>Connect with me!</h2>
+        <div style={{display:'flex', flexDirection: 'row', justifyContent: 'center'}}>
+        {
+          SOCIALPROFILES.map(PROFILE => {
+            return (
+              <div key={PROFILE.id}>
+              <Profile  profile={PROFILE}/>
+              </div>
+            )
+          }
+        )
+        }
+        </div>
+    </div>
+
+    )
+
+
+
+
+export default Profiles;
---
+++ portfoliov4/src/data/socialProfiles.js
@@ -0,0 +1,30 @@
+import emailIcon from '../assets/email_icon.png';
+import githubIcon from '../assets/github_icon.png';
+import linkedinIcon from '../assets/linkedin_icon.png';
+
+
+const SOCIALPROFILES = [
+  {
+      id: 1,
+      title: 'email',
+      description: 'A React App that I built, involving JS and core web dev concepts!',
+      link: 'https://github.com/15Dkatz/example',
+      image: emailIcon
+    },
+    {
+      id: 2,
+      title: 'github',
+      description: 'A REST API that I built from scratch with GET and POST requests!',
+      link: 'https://github.com/15Dkatz/example',
+      image: githubIcon
+    },
+    {
+      id: 3,
+      title: 'linkedin',
+      description: 'My unique final project for my university Operating Systems course.',
+      link: 'https://github.com/15Dkatz/example',
+      image: linkedinIcon
+    }
+];
+
+export default SOCIALPROFILES;
