86662f18daab17bb459be1b201a92074a83724b0
ee7klt <kltah1@gmail.com>
2020-04-02 16:38:44 +0800

    mapDispatchToProps

--- evens-or-odds/dist/src.e31bb0bc.js
+++ evens-or-odds/dist/src.e31bb0bc.js
@@ -34623,15 +34623,13 @@ function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _d
 
 function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
 
-function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
-
 function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+
+function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
 
 function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
-
-function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
 
 var App =
 /*#__PURE__*/
@@ -34639,27 +34637,9 @@ function (_Component) {
   _inherits(App, _Component);
 
   function App() {
-    var _getPrototypeOf2;
-
-    var _this;
-
     _classCallCheck(this, App);
 
-    for (var _len = arguments.length, args = new Array(_len), _key = 0; _key < _len; _key++) {
-      args[_key] = arguments[_key];
-    }
-
-    _this = _possibleConstructorReturn(this, (_getPrototypeOf2 = _getPrototypeOf(App)).call.apply(_getPrototypeOf2, [this].concat(args)));
-
-    _defineProperty(_assertThisInitialized(_this), "startGame", function () {
-      _this.props.dispatch((0, _settings.startGame)());
-    });
-
-    _defineProperty(_assertThisInitialized(_this), "cancelGame", function () {
-      _this.props.dispatch((0, _settings.cancelGame)());
-    });
-
-    return _this;
+    return _possibleConstructorReturn(this, _getPrototypeOf(App).apply(this, arguments));
   }
 
   _createClass(App, [{
@@ -34667,9 +34647,9 @@ function (_Component) {
     value: function render() {
       console.log('this', this);
       return _react.default.createElement("div", null, _react.default.createElement("h2", null, "Evens or Odds"), this.props.gameStarted ? _react.default.createElement("div", null, _react.default.createElement("h3", null, "The game is on!"), _react.default.createElement("br", null), _react.default.createElement("button", {
-        onClick: this.cancelGame
+        onClick: this.props.cancelGame
       }, "Cancel Game")) : _react.default.createElement("div", null, _react.default.createElement("h3", null, "A new game awaits"), _react.default.createElement("br", null), _react.default.createElement("button", {
-        onClick: this.startGame
+        onClick: this.props.startGame
       }, "Start Game")));
     }
   }]);
@@ -34684,7 +34664,18 @@ var mapStateToProps = function mapStateToProps(state) {
   };
 };
 
-var componentConnector = (0, _reactRedux.connect)(mapStateToProps);
+var mapDispatchToProps = function mapDispatchToProps(dispatch) {
+  return {
+    startGame: function startGame() {
+      return dispatch((0, _settings.startGame)());
+    },
+    cancelGame: function cancelGame() {
+      return dispatch((0, _settings.cancelGame)());
+    }
+  };
+};
+
+var componentConnector = (0, _reactRedux.connect)(mapStateToProps, mapDispatchToProps);
 
 var _default = componentConnector(App);
 
@@ -34786,7 +34777,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59043" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59260" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
--- evens-or-odds/src/components/App.js
+++ evens-or-odds/src/components/App.js
@@ -6,12 +6,7 @@ import {cancelGame,startGame} from '../actions/settings'
 
 
 class App extends Component {
-  startGame = () => {
-    this.props.dispatch(startGame());
-  }
-  cancelGame = () => {
-    this.props.dispatch(cancelGame());
-  }
+
 
   render() {
     console.log('this',this);
@@ -23,13 +18,13 @@ class App extends Component {
           <div>
             <h3>The game is on!</h3>
             <br />
-            <button onClick={this.cancelGame}>Cancel Game</button>
+            <button onClick={this.props.cancelGame}>Cancel Game</button>
           </div>
         ) : (
           <div>
           <h3>A new game awaits</h3>
           <br />
-          <button onClick={this.startGame}>Start Game</button>
+          <button onClick={this.props.startGame}>Start Game</button>
           </div>
         )
       }
@@ -46,8 +41,14 @@ const mapStateToProps = state => {
   }
 }
 
+const mapDispatchToProps = dispatch => {
+  return {
+    startGame: () => dispatch(startGame()),
+    cancelGame: () => dispatch(cancelGame())
+  }
+}
 
-const componentConnector = connect(mapStateToProps);
+const componentConnector = connect(mapStateToProps, mapDispatchToProps);
 
 
 export default componentConnector(App);
